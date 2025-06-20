ff48127a1740cef6a3a8a48ecc0865f24b23a07c
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 05:08:06 +0000

    [automatic commit] 2025-06-20 05:08:06 UTC

--- part1/app.js
+++ part1/app.js
@@ -163,22 +163,14 @@ let db;
     // copied from dummyrating.sql
     await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-        VALUES
-        (
+        VALUES (
             (SELECT dog_id FROM Dogs WHERE name = 'Max'),
             '2025-06-09 08:00:00',
             30,
             'Testplace1',
             'completed'
-        ),
-        (
-            (SELECT dog_id FROM Dogs WHERE name = 'Milo'),
-            '2025-06-09 09:00:00',
-            60,
-            'Testplace2',
-            'completed'
-        );
-    `);
+        )
+        `);
 
   } catch (err) {
 
