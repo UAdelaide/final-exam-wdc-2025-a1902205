e667b8e46cf3842e54aa69689fc2ee692f3dde55
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 04:54:03 +0000

    [automatic commit] 2025-06-20 04:54:02 UTC

--- part1/app.js
+++ part1/app.js
@@ -102,7 +102,7 @@ let db;
         )
     `);
 
-    // Insert data
+    // Insert data as described in 
     await db.execute(`
         INSERT INTO Users (username, email, password_hash, role)
         VALUES
@@ -114,9 +114,23 @@ let db;
     `);
 
     await db.execute(`
+        INSERT INTO Dogs (owner_id, name, size)
+        VALUES
+            ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+            ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+            ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Rocky', 'large'),
+            ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Lucy', 'small'),
+            ((SELECT user_id FROM Users WHERE username = 'ownerJane'), 'Milo', 'small');
 
     `);
     await db.execute(`
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        VALUES
+            ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Lucy'), '2025-06-10 10:30:00', 45, 'Testplace1', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-10 11:30:00', 45, 'Testplace2', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-10 12:30:00', 45, 'Testplace2', 'accepted');
 
     `);
 
