244a41d855e43f7852d9c024494ccb023da8a7f3
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 04:32:43 +0000

    [automatic commit] 2025-06-20 04:32:43 UTC

--- part1/app.js
+++ part1/app.js
@@ -77,18 +77,19 @@ app.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await db.execute(`
         SELECT
-    d.name AS dog_name,
-    d.size,
-    u.username AS owner_username
-FROM
-    Dogs d
-JOIN
-    Users u ON d.owner_id = u.user_id`);
-    res.json(books);
+            d.name AS dog_name,
+            d.size,
+            u.username AS owner_username
+        FROM
+            Dogs d
+        JOIN
+            Users u ON d.owner_id = u.user_id`);
+    res.json(rows);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
 
 module.exports = app;
+
