7b2c4e86994ef8be70ab0f209cf5ee1be0e18854
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 03:48:07 +0000

    [automatic commit] 2025-06-20 03:48:07 UTC

--- part1/dummyrating.sql
+++ part1/dummyrating.sql
@@ -5,13 +5,15 @@ VALUES
     (SELECT dog_id FROM Dogs WHERE name = 'Max'),
     '2025-06-09 08:00:00',
     30,
-    'Green Park',
+    'te',
     'completed'
 ),
 (
     (SELECT dog_id FROM Dogs WHERE name = 'Milo'),
     '2025-06-09 09:00:00',
     60,
-    'Riverside Trail',
+    '',
     'completed'
 );
+
+
