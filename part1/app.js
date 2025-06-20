a587c01f38b8bff22af675b1250b02cfd8bdca9f
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 05:26:34 +0000

    [automatic commit] 2025-06-20 05:26:34 UTC

--- part1/app.js
+++ part1/app.js
@@ -283,7 +283,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
             wr.status = 'open'`);
     res.json(rows);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
+    res.status(500).json({ error: 'Failed to fetch open requests' });
   }
 });
 
