8f7f7aa241e743cc953be2bf9fced816b6ac10cd
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 05:11:32 +0000

    [automatic commit] 2025-06-20 05:11:32 UTC

--- part1/app.js
+++ part1/app.js
@@ -203,7 +203,21 @@ let db;
         )
     `);
 
-    
+    await db.execute(`
+        INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
+VALUES(
+    (SELECT request_id from WalkRequests WHERE dog_id = (SELECT dog_id from Dogs WHERE
+    name = 'Max') AND requested_time = '2025-06-09 08:00:00'),
+    (SELECT user_id FROM Users WHERE username = 'bobwalker'),
+    (SELECT user_id FROM Users WHERE username = 'alice123'),
+    5,
+    'empty'
+)
+    `);
+
+    await db.execute(`
+    `);
+
 
 
 
