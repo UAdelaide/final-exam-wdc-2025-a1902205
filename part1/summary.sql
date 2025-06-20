3a3cdfe6e923ada178c85288ed486e39a80e6cfd
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 03:42:38 +0000

    [automatic commit] 2025-06-20 03:42:38 UTC

--- part1/summary.sql
+++ part1/summary.sql
@@ -1,4 +1,12 @@
 SELECT
     u.username AS walker_username,
     COUNT(r.rating_id) AS total_ratings,
-    AVG(r.rating) AS average_rating,
+    AVG(r.rating) AS average_rating
+FROM
+    Users u
+LEFT JOIN
+    WalkRatings r ON u.user_id = r.walker_id
+WHERE
+    u.role = 'walker'
+GROUP BY
+    u.user_id, u.username;
