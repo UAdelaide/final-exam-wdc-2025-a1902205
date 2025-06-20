5b4dc3be2bb4e9b03f95b572523a5cf74c55a548
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 05:08:47 +0000

    [automatic commit] 2025-06-20 05:08:47 UTC

--- part1/app.js
+++ part1/app.js
@@ -175,10 +175,10 @@ let db;
     await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES (
-            (SELECT dog_id FROM Dogs WHERE name = 'Max'),
-            '2025-06-09 08:00:00',
-            30,
-            'Testplace1',
+           (SELECT dog_id FROM Dogs WHERE name = 'Milo'),
+            '2025-06-09 09:00:00',
+            60,
+            'Testplace2',
             'completed'
         )
     `);
