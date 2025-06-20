d92c922490c5d0e61f4908f2ac963877fb4a85e9
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 08:14:14 +0000

    [automatic commit] 2025-06-20 08:14:14 UTC

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// added q 13
 // Session setup
 app.use(session({
   secret: 'yourSecretKey',
