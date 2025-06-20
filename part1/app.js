9d63093a68a01b2487ae113e31983c917bb69844
Qianyi Lyu <qianyi.lyu@student.adelaide.edu.au>
2025-06-20 04:53:21 +0000

    [automatic commit] 2025-06-20 04:53:21 UTC

--- part1/app.js
+++ part1/app.js
@@ -104,7 +104,13 @@ let db;
 
 // Insert data
     await db.execute(`
-
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES
+            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('ownerJane', 'jane@example.com', 'hashed123', 'owner'),
+            ('walkerMike', 'ike@example.com', 'hashed456', 'walker')
     `);
     await db.execute(`
 
